9a8aa8ced2f34af62dff4bb62e38ec3dc37a17c0
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2021-08-08 21:49:27 +0300

    Сейф 2

--- src/com/test/six/Main.java
+++ src/com/test/six/Main.java
@@ -1,13 +1,14 @@
 package com.test.six;
 
 public class Main {
-    public static  void main(String[] args) {
-        StorageItems storageItems = new StorageItems(new Item("сало",15,10.30),new Item("часы",1,30),
-                new Item("компас",1,4.20),new Item("сапоги",2,10.90),new Item("ружьё",2,110.10),
-                new Item("патроны",2,25.25),new Item("хлеб",1,5.0),new Item("бронежелет",18,75.60));
+    public static void main(String[] args) {
+        StorageItems storageItems = new StorageItems(new Item("колье", 15, 10.30),
+                new Item("часы", 1, 30), new Item("акции", 1, 4.20),
+                new Item("брошка", 2, 10.90), new Item("пистолет", 2, 110.10),
+                new Item("кольцо", 2, 25.25), new Item("акции", 1, 5.0),
+                new Item("слиток", 18, 75.60));
 
         Bag bag = new Bag(20);
-
         bag.putInBag(storageItems.ITEMS);
     }
 }
