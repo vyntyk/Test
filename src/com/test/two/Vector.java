ec709a4f4337da0f8ea945be63292ca26e35db6d
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2021-08-09 16:47:48 +0300

    Вектор

---
+++ src/com/test/two/Vector.java
@@ -0,0 +1,58 @@
+package com.test.two;
+import java.util.Scanner;
+
+public class Vector {
+    private int dx, dy;
+
+    Vector() {
+        dx = dy = 0;
+    }
+    Vector(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public double getLength() {
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public int Scalar(Vector v) {
+        return v.dx * dx + v.dy * dy;
+    }
+
+    public double GetAngle(Vector v) {
+        int s = this.Scalar(v);
+        return Math.acos(s/this.getLength() / v.getLength());
+    }
+}
+class Main {
+
+    public static void main(String[] args) {
+        Scanner con = new Scanner(System.in);
+        System.out.println("Введите координаты первого вектора");
+        // первый вектор - координаты точек
+        int a1 = Integer.parseInt(con.next());
+        int b1 = Integer.parseInt(con.next());
+        int a2 = Integer.parseInt(con.next());
+        int b2 = Integer.parseInt(con.next());
+        int a = a2-a1;
+        int b = b2-b1;
+        Vector u = new Vector(a, b);
+
+        System.out.println("Введите координаты второго вектора");
+        // второй вектор - координаты точек
+        int a3 = Integer.parseInt(con.next());
+        int b3 = Integer.parseInt(con.next());
+        int a4 = Integer.parseInt(con.next());
+        int b4 = Integer.parseInt(con.next());
+        a = a4-a3;
+        b = b4-b3;
+        Vector v = new Vector(a, b);
+
+        System.out.println(u.Scalar(v));
+        System.out.println(u.GetAngle(v));
+
+        con.close();
+    }
+}
+
